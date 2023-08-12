fd1f79a424cac0d1eabaf7cfff2ff3001993d02c
wangchenfei7788 <2717410751@qq.com>
2023-08-12 15:13:02 +0800

    wms first init

---
+++ wms-web/src/components/MainHome.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: "MainHome",
+  data() {
+    const item = {
+      date: '2016-05-02',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄'
+    };
+    return {
+      tableData: Array(10).fill(item)
+    }
+  }
+}
+</script>
+
+<template>
+  <el-table :data="tableData">
+    <el-table-column prop="date" label="日期" width="140">
+    </el-table-column>
+    <el-table-column prop="name" label="姓名" width="120">
+    </el-table-column>
+    <el-table-column prop="address" label="地址">
+    </el-table-column>
+  </el-table>
+</template>
+
+<style scoped>
+
+</style>
---
+++ wms/src/main/java/com/wms/service/UserService.java
@@ -0,0 +1,23 @@
+package com.wms.service;
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.wms.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author wms
+ * @since 2023-08-10
+ */
+public interface UserService extends IService<User> {
+
+    IPage pageC(IPage<User> page);
+
+    IPage pageCC(IPage<User>page, Wrapper wrapper);
+}
