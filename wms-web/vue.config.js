31bd07af94a7f65685ebad5528654d9bb446c479
wangchenfei7788 <2717410751@qq.com>
2023-09-23 22:40:11 +0800

    wms init

--- wms-web/vue.config.js
+++ wms-web/vue.config.js
@@ -1,6 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer:{
+    historyApiFallback: true,
+    allowedHosts: "all",
+    client: {
+      webSocketURL: 'wss://localhost:8080/',
+    },
+  }
 
 })
 
